61028dcbbf9662c77f5d99f9903154547c766bad
Maciej Czuchnowski <mczuchnowski@users.noreply.github.com>
2015-03-15 02:09:08 +0100

    Refactor dashboard spec to use env variables

--- spec/support/auth_helper.rb
+++
@@ -1,7 +0,0 @@
-module AuthHelper
-  def http_login
-    name = 'admin'
-    password = 'admin'
-    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(name,password)
-  end
-end
