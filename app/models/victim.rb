0e3a923cafc990ad6e866fffb12cfac985911067
megamos <whydoyoudothethingsudo@hotmail.com>
2015-06-09 00:22:49 +0200

    Added header texts to charts. More styling done.

--- app/models/victim.rb
+++ app/models/victim.rb
@@ -30,7 +30,7 @@ class Victim < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 5, maximum: 100}
   validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 120 }
   validates :country, presence: true
-  validates :city, presence: true, length: {maximum: 30}
+  validates :city, length: {maximum: 30}
   validates :link, presence: true
   validates :info, length: {maximum: 700}
 
